9b970c586afda8faf1779d7756f1f6440823d7eb
SANGAMARTIN99 <martinsanga230@gmail.com>
2025-10-23 12:08:40 +0300

    remove apiURL

--- src/api/client.ts
+++ src/api/client.ts
@@ -7,8 +7,7 @@ import { REFRESH_TOKEN } from '../api/mutations';
 import { ENDPOINT } from '../api/environment';
 
 const httpLink = createHttpLink({
-  uri: ENDPOINT.apiUrl,
-  credentials: 'include', // Important for cookies if using them
+  uri: `${ENDPOINT}`,
 });
 
 // Add the JWT token to the headers
