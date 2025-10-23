49eb9ae7c460f7e840a172850659d87f0673fdfb
SANGAMARTIN99 <martinsanga230@gmail.com>
2025-10-23 09:10:48 +0300

    updates on rendering

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,22 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  server: {
+    headers: {
+      'Content-Type': 'text/javascript',
+    },
+  },
+  build: {
+    assetsInlineLimit: 0, // Ensure all assets are inlined
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
 })
