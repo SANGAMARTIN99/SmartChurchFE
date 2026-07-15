7c7c59e48d4322f4540e5be4490c2f58acd77d99
SANGAMARTIN99 <martinsanga230@gmail.com>
2026-07-15 11:45:04 +0300

    Optimize background GSAP animation timelines

--- src/pages/MEMBER/My-Offerings-Overview.tsx
+++ src/pages/MEMBER/My-Offerings-Overview.tsx
@@ -734,3 +734,5 @@ export default MyOfferingsOverview;
 // Revision note [2026-07-17 09:36:11 +0300]: Optimize background GSAP animation timelines
 
 // Revision note [2026-07-31 14:13:35 +0300]: Update dropdown selector options and hints
+
+// Activity update [2026-07-15 11:45:04 +0300]: Optimize background GSAP animation timelines
