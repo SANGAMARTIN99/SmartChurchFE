05fc6fee387b974c8e3717ad557653112202a6d7
SANGAMARTIN99 <martinsanga230@gmail.com>
2026-07-16 15:40:53 +0300

    Refactor card application status badges

--- src/pages/MEMBER/GetCard.tsx
+++ src/pages/MEMBER/GetCard.tsx
@@ -356,3 +356,5 @@ export default GetCard;
 // Revision note [2026-07-18 18:34:33 +0300]: Refactor card application status badges
 
 // Revision note [2026-08-02 09:43:25 +0300]: Enhance form input validation and feedback
+
+// Activity update [2026-07-16 15:40:53 +0300]: Refactor card application status badges
