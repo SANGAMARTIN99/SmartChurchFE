1ded1ebc038b8b55cc97c6d4751baa30bfdd5ebc
SANGAMARTIN99 <martinsanga230@gmail.com>
2026-07-18 18:20:53 +0300

    Update offering cards registration window view

--- src/pages/PASTOR/PastorAnnouncements.tsx
+++ src/pages/PASTOR/PastorAnnouncements.tsx
@@ -513,3 +513,5 @@ export default AnnouncementsPage;
 // Revision note [2026-07-20 14:40:42 +0300]: Update offering cards registration window view
 
 // Revision note [2026-08-03 18:42:35 +0300]: Enhance blog feed pagination control
+
+// Activity update [2026-07-18 18:20:53 +0300]: Update offering cards registration window view
