dfc76f0c7d332d05ba3b660920bdbe74f2a4959c
SANGAMARTIN99 <martinsanga230@gmail.com>
2026-07-24 17:14:00 +0300

    Improve responsive grid breakpoint spacing

--- src/pages/SECRETARY/OfferingCards.tsx
+++ src/pages/SECRETARY/OfferingCards.tsx
@@ -560,3 +560,5 @@ export default OfferingCardsPage;
 // Revision note [2026-07-30 18:44:10 +0300]: Refactor footer social links and text styling
 
 // Activity update [2026-07-14 13:54:22 +0300]: Update word of the day dynamic graphics
+
+// Activity update [2026-07-24 17:14:00 +0300]: Improve responsive grid breakpoint spacing
