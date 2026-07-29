a9e665d32b0dad07192a6653ad8ece1c85eb576e
SANGAMARTIN99 <martinsanga230@gmail.com>
2026-07-29 18:37:41 +0300

    Refactor token refresh error handler

--- src/pages/BLOG/components/PendingPostCard.tsx
+++ src/pages/BLOG/components/PendingPostCard.tsx
@@ -78,3 +78,5 @@ const PendingPostCard = ({ post, refetch }: PendingPostCardProps) => {
 export default PendingPostCard;
 
 // Revision note [2026-07-15 14:27:18 +0300]: Update pastor dashboard group management UI
+
+// Revision note [2026-07-29 18:37:41 +0300]: Refactor token refresh error handler
