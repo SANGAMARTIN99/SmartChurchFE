9a8b07a1a9fd645f6f216438de943aecf0f16fd4
SANGAMARTIN99 <martinsanga230@gmail.com>
2026-08-02 09:43:25 +0300

    Enhance form input validation and feedback

--- src/pages/MEMBER/GetCard.tsx
+++ src/pages/MEMBER/GetCard.tsx
@@ -354,3 +354,5 @@ const GetCard = () => {
 export default GetCard;
 
 // Revision note [2026-07-18 18:34:33 +0300]: Refactor card application status badges
+
+// Revision note [2026-08-02 09:43:25 +0300]: Enhance form input validation and feedback
