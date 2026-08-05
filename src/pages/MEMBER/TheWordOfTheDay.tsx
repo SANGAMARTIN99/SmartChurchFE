15da9af0beebb56579852352e9a3ab65435c4e87
SANGAMARTIN99 <martinsanga230@gmail.com>
2026-08-05 14:01:11 +0300

    Enhance table column sorting indicator UI

--- src/pages/MEMBER/TheWordOfTheDay.tsx
+++ src/pages/MEMBER/TheWordOfTheDay.tsx
@@ -327,3 +327,5 @@ export default TheWordOfTheDay;
 // Activity update [2026-07-15 21:23:36 +0300]: Enhance member contribution table filters
 
 // Activity update [2026-07-26 11:12:34 +0300]: Refactor footer social links and text styling
+
+// Activity update [2026-08-05 14:01:11 +0300]: Enhance table column sorting indicator UI
